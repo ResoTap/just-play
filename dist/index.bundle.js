a01f661a1345d83c4ef60b26103f7b9c5399c63c
Patrick Oser <patrick.oser148@gmail.com>
2023-08-06 14:47:27 -0400

    Small changes.

---
+++ dist/index.bundle.js
@@ -0,0 +1,19 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBL9FckSYL0lCQ6sM5fW2kQL7SLI0zpHfg",
+  authDomain: "just-play-ec9eb.firebaseapp.com",
+  projectId: "just-play-ec9eb",
+  storageBucket: "just-play-ec9eb.appspot.com",
+  messagingSenderId: "54544685025",
+  appId: "1:54544685025:web:4b2573ffbd224d8a78e4a0",
+  measurementId: "G-Z7DVDGRXLQ",
+};
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9qdXN0LXBsYXkvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gRm9yIEZpcmViYXNlIEpTIFNESyB2Ny4yMC4wIGFuZCBsYXRlciwgbWVhc3VyZW1lbnRJZCBpcyBvcHRpb25hbFxuY29uc3QgZmlyZWJhc2VDb25maWcgPSB7XG4gIGFwaUtleTogXCJBSXphU3lCTDlGY2tTWUwwbENRNnNNNWZXMmtRTDdTTEkwenBIZmdcIixcbiAgYXV0aERvbWFpbjogXCJqdXN0LXBsYXktZWM5ZWIuZmlyZWJhc2VhcHAuY29tXCIsXG4gIHByb2plY3RJZDogXCJqdXN0LXBsYXktZWM5ZWJcIixcbiAgc3RvcmFnZUJ1Y2tldDogXCJqdXN0LXBsYXktZWM5ZWIuYXBwc3BvdC5jb21cIixcbiAgbWVzc2FnaW5nU2VuZGVySWQ6IFwiNTQ1NDQ2ODUwMjVcIixcbiAgYXBwSWQ6IFwiMTo1NDU0NDY4NTAyNTp3ZWI6NGIyNTczZmZiZDIyNGQ4YTc4ZTRhMFwiLFxuICBtZWFzdXJlbWVudElkOiBcIkctWjdEVkRHUlhMUVwiLFxufTtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
