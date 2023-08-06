4388e7368e78860e1e3917c1ff31d399e0e1c2f7
Patrick Oser <patrick.oser148@gmail.com>
2023-08-06 15:23:41 -0400

    Set up firebase in index.js.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { initializeApp } from "firebase/app";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBL9FckSYL0lCQ6sM5fW2kQL7SLI0zpHfg",
@@ -8,3 +10,5 @@ const firebaseConfig = {
   appId: "1:54544685025:web:4b2573ffbd224d8a78e4a0",
   measurementId: "G-Z7DVDGRXLQ",
 };
+
+initializeApp(firebaseConfig);
