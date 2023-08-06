036c600b30d1ab071890a411ee98a256869fb13f
Patrick Oser <patrick.oser148@gmail.com>
2023-08-06 13:32:07 -0400

    Add firebase config to index.js.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("hello, index.js");
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBL9FckSYL0lCQ6sM5fW2kQL7SLI0zpHfg",
+  authDomain: "just-play-ec9eb.firebaseapp.com",
+  projectId: "just-play-ec9eb",
+  storageBucket: "just-play-ec9eb.appspot.com",
+  messagingSenderId: "54544685025",
+  appId: "1:54544685025:web:4b2573ffbd224d8a78e4a0",
+  measurementId: "G-Z7DVDGRXLQ",
+};
