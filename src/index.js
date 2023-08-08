81b8a3870a333befea921c58e5d9af241735739c
Patrick Oser <patrick.oser148@gmail.com>
2023-08-08 10:37:39 -0400

    Access firestore collection data established.

--- src/index.js
+++ src/index.js
@@ -26,6 +26,11 @@ const db = getFirestore();
 const colRef = collection(db, "users");
 
 // get collection data
-getDocs(colRef).then((snapshot) => {
-  console.log(snapshot.docs);
+getDocs(colRef)
+  .then((snapshot) => {
+    let users = [];
+    snapshot.docs.forEach((doc) => {
+      users.push({ ...doc.data(), id: doc.id })
+  })
+  console.log(users);
 });
