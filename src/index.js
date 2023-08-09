6806ca6546a07e2354d00938012fa1f0a8fbe24b
Patrick Oser <patrick.oser148@gmail.com>
2023-08-09 13:07:18 -0400

    'users' array updates upon change with new collection data.

--- src/index.js
+++ src/index.js
@@ -25,13 +25,12 @@ const db = getFirestore();
 // collection ref
 const colRef = collection(db, "users");
 
-// get collection data
-getDocs(colRef)
-  .then((snapshot) => {
-    let users = [];
-    snapshot.docs.forEach((doc) => {
-      users.push({ ...doc.data(), id: doc.id })
-  })
+// real time collection data
+onSnapshot(colRef, (snapshot) => {
+  let users = [];
+  snapshot.docs.forEach((doc) => {
+    users.push({ ...doc.data(), id: doc.id })
+})
   console.log(users);
 });
 
